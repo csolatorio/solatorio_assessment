8751c10ad36ab1a00dab8ae16c16a97be576a60f
csolatorio <46466477+csolatorio@users.noreply.github.com>
2022-01-12 15:42:27 +0800

    Add files via upload

---
+++ include/forget_password.php
@@ -0,0 +1,35 @@
+
+
+<div class="col-md-6">                    
+	<div class="panel panel-info" >
+		<div class="panel-heading">
+			<div class="panel-title">Forgot Password</div>                        
+		</div> 
+		<div style="padding-top:30px" class="panel-body" >
+			<?php if ($message != '') { ?>
+				<div id="login-alert" class="alert alert-danger col-sm-12"><?php echo $message; ?></div>                            
+			<?php } ?>
+			<?php if(!empty($_GET['authtoken']) && $_GET['authtoken']) { ?>
+				<form id="loginform" class="form-horizontal" role="form" method="POST" action="">                                    
+					<div style="margin-bottom: 25px" class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+						<input type="password" class="form-control" id="password" name="password"  placeholder="New password..." required>			
+					</div>
+					<div style="margin-bottom: 25px" class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+						<input type="password" class="form-control" id="cpassword" name="cpassword"  placeholder="Confirm password..." required>              
+					</div>	
+					<div style="margin-top:10px" class="form-group">                               
+						<div class="col-sm-12 controls">
+							<input type="hidden" name="authtoken"  value="<?php echo $_GET['authtoken']; ?>" />
+							<input type="submit" name="resetpassword" value="Save" class="btn btn-info">						  
+						</div>						
+					</div>					 
+					</div>  	
+				</form>
+			<?php } else { ?>
+				Invalid password reset request.
+			<?php } ?>
+		</div>                     
+	</div>  
+</div>
